96aa6d9c0e5f410d560a8a1cadbc4afc3d559b4b
Eric Scott <scottzillaster@gmail.com>
2020-04-24 14:16:44 -0500

    fixed error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
